6ff3eb62302e43cffb8c673005f6feb92790354c
Anthony Chavis <gitanthony@yahoo.com>
2023-10-11 11:19:20 -0400

    word wrap

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './Name.module.css';
 
 function Name({ name }: { name: string }) {
-    return <h2>{name}</h2>;
+    return (
+        <h2 className={styles.wordWrap} aria-label="username">
+            {name}
+        </h2>
+    );
 }
 
 export default Name;
